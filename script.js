bd1aec7c9df39ab07d10d303d7b34aa2ba6440cc
iram <marius.hoehn@gmail.com>
2025-11-10 13:20:18 +0100

    filter function added

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ const itemInput = document.getElementById("item-input");
 const itemList = document.getElementById("item-list");
 const clear = document.getElementById("clear");
 const itemFilter = document.getElementById("filter");
-const items = itemList.querySelectorAll("li");
 
 function addItem(e) {
   e.preventDefault();
@@ -46,7 +45,10 @@ function createIcon(classes) {
 
 function removeItem(e) {
   if (e.target.parentElement.classList.contains("remove-item")) {
-    e.target.parentElement.parentElement.remove();
+    if (confirm("Are you sure?")) {
+      e.target.parentElement.parentElement.remove();
+      checkUI();
+    }
   }
 }
 
@@ -54,9 +56,26 @@ function clearItems() {
   while (itemList.firstChild) {
     itemList.removeChild(itemList.firstChild);
   }
+  checkUI();
+}
+
+function filterItems(e) {
+  const items = document.querySelectorAll("li");
+  const text = e.target.value.toLowerCase();
+
+  items.forEach((item) => {
+    const itemName = item.firstChild.textContent.toLowerCase();
+
+    if (itemName.indexOf(text) != -1) {
+      item.style.display = "flex";
+    } else {
+      item.style.display = "none";
+    }
+  });
 }
 
 function checkUI() {
+  const items = itemList.querySelectorAll("li");
   if (items.length === 0) {
     clear.style.display = "none";
     itemFilter.style.display = "none";
@@ -70,5 +89,6 @@ function checkUI() {
 itemForm.addEventListener("submit", addItem);
 itemList.addEventListener("click", removeItem);
 clear.addEventListener("click", clearItems);
+itemFilter.addEventListener("input", filterItems);
 
 checkUI();
